3c15451cb37537a954bb304dce274d7a3b9c0627
ivan levy <ivo.levy03@gmail.com>
2025-12-25 17:19:53 -0300

    a

--- frontend/src/components/TableViewB2B.jsx
+++ frontend/src/components/TableViewB2B.jsx
@@ -386,8 +386,6 @@ function TableViewB2B({
           )}
         </select>
 
-        <div className="filter-checkboxes-and-actions">
-
         {hayFiltrosActivos && (
           <button 
             type="button" 
@@ -401,7 +399,6 @@ function TableViewB2B({
             </svg>
           </button>
         )}
-          </div>
 
         <div className="filter-actions-right">
           {isPro ? (
@@ -451,7 +448,6 @@ function TableViewB2B({
               <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
             </svg>
           </button>
-          </div>
         </div>
       </div>
 
