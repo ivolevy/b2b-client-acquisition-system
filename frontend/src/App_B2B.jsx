ec2e73b04023ca2a9c610eb0302c76aa00192281
ivan levy <ivo.levy03@gmail.com>
2026-02-11 16:46:01 -0300

    template

--- frontend/src/App_B2B.jsx
+++ frontend/src/App_B2B.jsx
@@ -843,6 +843,7 @@ function AppB2B() {
       )}
 
       <ToastContainer toasts={toasts} onRemove={removeToast} />
+      <ReactToastifyContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
       
       {/* Eliminado badge PRO flotante */}
     </div>
