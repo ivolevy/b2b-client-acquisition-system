bf8e5bbe30a6af9f6f607e222507c1608b0d1dbe
ivan levy <ivo.levy03@gmail.com>
2026-08-18 21:59:08 -0300

    Fix: Correct relative import path for authStorage in api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
   },
 });
 
-import { authStorage } from './utils/storage';
+import { authStorage } from '../utils/storage';
 
 api.interceptors.request.use((config) => {
   const token = authStorage.getToken();
